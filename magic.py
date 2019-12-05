aabd19ef0869b1ee093f0ec91f783c4e25bc03b4
MJL7068 <mattil@netti.fi>
2019-12-05 16:44:07 +0200

    added documentation

--- magic.py
+++ magic.py
@@ -6,6 +6,8 @@ from surprise import SVD
 from surprise import Dataset
 from surprise import Reader
 
+# courses should be a list of names for the courses that the user wants grades for
+# id should be the same number that was used as id in update_model
 def get_grades(algo, id, courses):
   grades = {}
   for c in courses:
@@ -21,11 +23,17 @@ def get_fit(df):
   algo = SVD()
   return algo.fit(data.build_full_trainset())
 
+# course_grades_dict should be a python dictionary where keys are the course names
+# and values are grades from those courses
+# id should be a number
+# This function adds the courses that the student has done into the pandas dataframe
 def update_model(df, id, course_grades_dict):
   for c in course_grades_dict.keys():
     df = df.append({'id': str(id), 'course': c, 'grade': str(course_grades_dict[c])}, ignore_index=True)
   return df
 
+# This loads a csv-file from a location defined by path
+# Returns the dataset as a pandas dataframe
 def load_data(path)
     df = pd.read_csv(path, sep = ",", parse_dates = True)
     del df['date']
