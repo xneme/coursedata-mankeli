071c1eb62846d67d45b5a05eab853e0d1a9f9404
Jere Helén <jere.helen@helsinki.fi>
2020-01-16 12:39:09 +0200

    Grades MVP

--- magic.py
+++ magic.py
@@ -6,41 +6,48 @@ from surprise import SVD
 from surprise import Dataset
 from surprise import Reader
 
-path = ''
+path = '/srv/flask_app/data/credits.csv'
 
 # courses_dict should be a python dictonary with course names as keys and grades as values
+
+
 def get_grades(course_grades_dict):
-  df = load_data(path)
-  df = update_data(df, "1", course_grades_dict)
-  algo = get_fit(df)
+    df = load_data(path)
+    df = update_data(df, "1", course_grades_dict)
+    algo = get_fit(df)
 
-  grades = {}
-  for c in np.unique(df.course):
-    result = algo.predict(str("1"), c)
-    grades[c] = result.est
+    grades = {}
+    for c in pd.unique(df.course):
+        result = algo.predict(str("1"), c)
+        grades[c] = result.est
 
-  return grades
-  }
+    return grades
+
 
 def get_fit(df):
-  reader = Reader(rating_scale=(1, 5))
-  data = Dataset.load_from_df(df[['id', 'course', 'grade']], reader)
-  algo = SVD()
-  return algo.fit(data.build_full_trainset())
+    reader = Reader(rating_scale=(1, 5))
+    data = Dataset.load_from_df(df[['id', 'course', 'grade']], reader)
+    algo = SVD()
+    return algo.fit(data.build_full_trainset())
 
 # course_grades_dict should be a python dictionary where keys are the course names
 # and values are grades from those courses
 # id should be a number
 # This function adds the courses that the student has done into the pandas dataframe
+
+
 def update_data(df, id, course_grades_dict):
-  for c in course_grades_dict.keys():
-    df = df.append({'id': str(id), 'course': c, 'grade': str(course_grades_dict[c])}, ignore_index=True)
-  return df
+    for c in course_grades_dict.keys():
+        df = df.append({'id': str(id), 'course': c, 'grade': str(
+            course_grades_dict[c])}, ignore_index=True)
+    return df
 
 # This loads a csv-file from a location defined by path
 # Returns the dataset as a pandas dataframe
-def load_data(path)
-    df = pd.read_csv(path, sep = ",", parse_dates = True)
+
+
+def load_data(path):
+    print(os.getcwd())
+    df = pd.read_csv(path, sep=",", parse_dates=True)
     del df['date']
     return df
-
--- mankeli.py
+++ mankeli.py
@@ -5,21 +5,36 @@ from magic import get_grades
 
 app = Flask(__name__)
 
-grades = get_grades()
 
-@app.route('/grades/<studentnumber>')
-def hello(studentnumber):
-  if (request.headers.get('Authorization') != os.environ.get('TOKEN')):
-    return jsonify({ 'error': 'Invalid token!'}), 403
+@app.route('/grades', methods=['POST'])
+def grades():
+    if (request.headers.get('Authorization') != os.environ.get('TOKEN')):
+        return jsonify({'error': 'Invalid token!'}), 403
+    grades = request.json
+    return jsonify(get_grades(grades))
 
-  return jsonify(grades[studentnumber])
+
+@app.route('/postgrades', methods=['POST'])
+def postgrades():
+    grades = request.json
+    return jsonify(get_grades(grades))
+
+
+@app.route('/getgrades', methods=['GET'])
+def getgrades():
+    grades = request.json
+    return jsonify(get_grades({"Tietorakenteet ja algoritmit": 5,
+                               "Ohjelmoinnin perusteet": 5,
+                               "Design and Analysis of Algorithms": 5}))
+
 
 @app.route('/ping')
 def pong():
-  return "pong!"
+    return "pong!"
+
 
 @app.route('/sample')
 def sample():
-  with open('data/sample.csv', 'r') as file:
-    reader = csv.reader(file, delimiter=';')
-    return jsonify(list(reader))
+    with open('data/sample.csv', 'r') as file:
+        reader = csv.reader(file, delimiter=';')
+        return jsonify(list(reader))
